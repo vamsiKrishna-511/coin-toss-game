181eff55e7d7ae9cdaa134ff00ab6ca5a4199fc3
vamsiKrishna-511 <devulapallivamsi197@gmail.com>
2023-04-26 13:59:29 +0000

    coin toss game

---
+++ src/components/CoinToss/index.js
@@ -0,0 +1,68 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+const HEADS_IMG_URL = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
+
+const TAILS_IMG_URL = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
+
+class CoinToss extends Component {
+  state = {
+    tossResultImage: HEADS_IMG_URL,
+    headsCount: 0,
+    tailsCount: 0,
+  }
+
+  onClickTossCoin = () => {
+    const {headsCount, tailsCount} = this.state
+    const tossResult = Math.floor(Math.random() * 2)
+    let tossImage = ''
+    let latestHeadCount = headsCount
+    let latestTailCount = tailsCount
+
+    if (tossResult === 0) {
+      tossImage = HEADS_IMG_URL
+      latestHeadCount += 1
+    } else {
+      tossImage = TAILS_IMG_URL
+      latestTailCount += 1
+    }
+
+    this.setState({
+      tossResultImage: tossImage,
+      headsCount: latestHeadCount,
+      tailsCount: latestTailCount,
+    })
+  }
+
+  render() {
+    const {tossResultImage, headsCount, tailsCount} = this.state
+    const totalCount = headsCount + tailsCount
+    return (
+      <div className="app-container">
+        <div className="coin-toss-container">
+          <h1 className="heading">Coin Toss Game</h1>
+          <p className="description">Heads (or) Tails</p>
+          <img
+            src={tossResultImage}
+            className="toss-result-image"
+            alt="toss results"
+          />
+          <button
+            type="button"
+            className="button"
+            onClick={this.onClickTossCoin}
+          >
+            Toss Coin
+          </button>
+          <div className="counts-container">
+            <p className="count">Total: {totalCount}</p>
+            <p className="count">Heads: {headsCount}</p>
+            <p className="count">Tails: {tailsCount}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default CoinToss
